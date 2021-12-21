8392436258cf669320c8cca88d1101591235ccb8
Steven Hugg <sehugg@users.noreply.github.com>
2021-12-21 12:05:48 -0600

    updated from 8bitworkshop.com

--- snow.c
+++ snow.c
@@ -6,23 +6,35 @@
 
 //#link "snowbmp.s"
 
+// bitmap bytes (from Dithertron)
 extern const char BITMAP[];
+
+// ported from Dithertron, draws the background bitmap
+// and sets the palette colors
 extern void LoadBitmap();
 
+// data structure for a single snowflake
 typedef struct Flake {
-  u8 x,y;
+  u8 x,y; // x,y position
 } Flake;
 
+// how many snowflakes?
 #define NUMFLAKES 120
 
+// 2-pixel byte that draws a single snowflake
+#define SNOWFLAKE_BYTE 0xb9
+
+// array of snowflakes
 Flake flakes[NUMFLAKES];
 
+// initialize a snowflake at a random position
 void InitFlake(Flake* f) {
   f->x = rand();
   f->y = rand();
 }
 
-// copy byte from source bitmap to screen
+// erase snowflake --
+// copy byte from background bitmap to screen
 void EraseFlake(Flake* f) {
   char* ptr;
   if (f->y < 192) {
@@ -31,24 +43,25 @@ void EraseFlake(Flake* f) {
   }
 }
 
-// set byte on screen
+// set snowflake byte on screen
 void DrawFlake(Flake* f) {
   char* ptr;
   if (f->y < 192) {
     ptr = cpct_getScreenPtr ((void*)0xc000, f->x, f->y);
-    *ptr = 0x31;
+    *ptr = SNOWFLAKE_BYTE;
   }
 }
 
+// make snowflake fall downward
 void MoveFlake(Flake* f) {
   f->y++;
 }
 
+// init snowflakes and make it snow forever
 void MakeItSnow(void) {
   for (int i=0; i<NUMFLAKES; i++) {
     Flake* f = &flakes[i];
     InitFlake(f);
-    //XorFlake(f);
   }
   while (1) {
     for (int i=0; i<NUMFLAKES; i++) {
@@ -60,6 +73,7 @@ void MakeItSnow(void) {
   }
 }
 
+// main routine
 void main(void) {
   
   cpct_setVideoMode(0);
